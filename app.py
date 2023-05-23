47598a076515d08c50f9d9cc001ee83c3f9ded86
KingBosu <mckaywilliamiii@gmail.com>
2023-05-23 17:34:01 -0400

    Added pokemon attack hp and defense stats

--- app.py
+++ app.py
@@ -24,8 +24,11 @@ def main():
         pokemon_ability = response.json()['abilities'][0]['ability']['name']
         pokemon_base_exp = response.json()['base_experience']
         pokemon_sprites = response.json()['sprites']['front_shiny']
+        pokemon_hp = response.json()['stats'][1]['base_stat']
+        pokemon_defense = response.json()['stats'][3]['base_stat']
+        pokemon_attack = response.json()['stats'][2]['base_stat']
         pokemon_data = {'Name of pokemon': pokemon, 'Pokemon Ability': pokemon_ability, 'Pokemon Base EXP': pokemon_base_exp, 'Pokemon Sprite': pokemon_sprites}
-        return render_template('main.html',pokemon_data=pokemon_data,pokemon=pokemon, pokemon_ability=pokemon_ability, pokemon_base_exp=pokemon_base_exp, pokemon_sprites=pokemon_sprites)
+        return render_template('main.html',pokemon_data=pokemon_data,pokemon=pokemon, pokemon_ability=pokemon_ability, pokemon_base_exp=pokemon_base_exp, pokemon_sprites=pokemon_sprites, pokemon_hp=pokemon_hp, pokemon_defense=pokemon_defense, pokemon_attack=pokemon_attack)
     
     return render_template('main.html')
 
